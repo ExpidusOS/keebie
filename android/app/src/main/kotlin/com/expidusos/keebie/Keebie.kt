71a8a09a450d31ec3bad43ac8566cf98a3af9d78
Tristan Ross <tristan.ross@midstall.com>
2023-06-02 16:45:21 -0700

    fix(android): size and positioning

--- android/app/src/main/kotlin/com/expidusos/keebie/Keebie.kt
+++ android/app/src/main/kotlin/com/expidusos/keebie/Keebie.kt
@@ -1,16 +1,26 @@
 package com.expidusos.keebie
 
 import android.inputmethodservice.InputMethodService
+import android.util.DisplayMetrics
 import android.view.View
 
 class Keebie: InputMethodService() {
     override fun onCreateInputView() : View {
         return layoutInflater.inflate(R.layout.keyboard_view, null).apply {
             requestLayout()
+            (this@Keebie).updateFullscreenMode()
         }
     }
 
     override fun onCreateExtractTextView() : View? {
         return null
+    }
+
+    override fun onEvaluateFullscreenMode() : Boolean {
+        return false
+    }
+
+    override fun isExtractViewShown(): Boolean {
+        return false
     }
 }
--- android/app/src/main/kotlin/com/expidusos/keebie/KeebieView.kt
+++ android/app/src/main/kotlin/com/expidusos/keebie/KeebieView.kt
@@ -29,16 +29,16 @@ class KeebieView(context: Context, attrs: AttributeSet) : ViewGroup(context, att
         context.display?.getMetrics(displayMetrics)
 
         var maxWidth = Math.max(width, displayMetrics.widthPixels)
-        var maxHeight = Math.max(height, (displayMetrics.heightPixels / 1.5).toInt())
+        var maxHeight = Math.max(height, (displayMetrics.heightPixels / 2).toInt())
 
         measureChild(view, width, height)
         setMeasuredDimension(
             resolveSizeAndState(maxWidth, width, view.measuredState),
-            resolveSizeAndState(maxHeight, height, view.measuredState)
+            resolveSizeAndState(maxHeight, height, view.measuredState shl MEASURED_HEIGHT_STATE_SHIFT)
         )
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        view.layout(left, top, right, bottom)
+        if (changed) view.layout(left, top, right, bottom)
     }
 }
